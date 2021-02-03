46e0ada02cf92599e1e28314a983968db3bfc9a9
Jonathan Choi <jisung594@gmail.com>
2021-02-03 12:35:57 -0500

    request api for current prices

--- tracer_be/retrieve/retrieve.py
+++ tracer_be/retrieve/retrieve.py
@@ -14,9 +14,17 @@ api_key = Config.API_KEY
 # STOCKS
 @retrieve_bp.route('/stocks_us', methods=['GET'])
 def list_stocks():
-    exchange= 'US' # *** placeholder ***
+    exchange = 'US' # *** placeholder ***
     return requests.get('https://finnhub.io/api/v1/stock/symbol?exchange=' + exchange + '&token=' + api_key).content
 
+
+@retrieve_bp.route('/current_price', methods=['GET','POST'])
+def get_price():
+    if request.method == 'POST':
+        stock = request.get_json()
+        ticker = stock['symbol']
+        return requests.get('https://finnhub.io/api/v1/quote?symbol=' + ticker + '&token=' + api_key).content
+# ------------------------------------------------------------------------------
 
 # CRYPTO
 @retrieve_bp.route('/crypto_exchanges', methods=['GET'])
@@ -28,7 +36,7 @@ def list_crypto():
     # exchange = request.form['exchange']
     exchange = 'kraken' # *** placeholder ***
     return requests.get('https://finnhub.io/api/v1/crypto/symbol?exchange=' + exchange + '&token=' + api_key).content
-
+# ------------------------------------------------------------------------------
 
 # FOREX
 @retrieve_bp.route('/fx_exchanges', methods=['GET'])

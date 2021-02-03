7dee43b40415897453494041e2122b7a245f138f
Jonathan Choi <jisung594@gmail.com>
2021-02-02 19:07:11 -0500

    routes for querying security by type

--- tracer_be/retrieve/retrieve.py
+++ tracer_be/retrieve/retrieve.py
@@ -7,7 +7,33 @@ retrieve_bp = Blueprint(
     'retrieve_bp', __name__
 )
 
-@retrieve_bp.route('/stocks', methods=['GET'])
+
+api_key = Config.API_KEY
+
+
+# STOCKS
+@retrieve_bp.route('/stocks_us', methods=['GET'])
 def list_stocks():
-    api_key = Config.API_KEY
-    return requests.get('https://finnhub.io/api/v1/stock/symbol?exchange=US&token=' + api_key).content
+    exchange= 'US' # *** placeholder ***
+    return requests.get('https://finnhub.io/api/v1/stock/symbol?exchange=' + exchange + '&token=' + api_key).content
+
+
+# CRYPTO
+@retrieve_bp.route('/crypto_exchanges', methods=['GET'])
+def crypto_exchanges():
+    return requests.get('https://finnhub.io/api/v1/crypto/exchange?token=' + api_key).content
+
+@retrieve_bp.route('/cryptos_by_exchange', methods=['GET','POST'])
+def list_crypto():
+    # exchange = request.form['exchange']
+    exchange = 'kraken' # *** placeholder ***
+    return requests.get('https://finnhub.io/api/v1/crypto/symbol?exchange=' + exchange + '&token=' + api_key).content
+
+
+# FOREX
+@retrieve_bp.route('/fx_exchanges', methods=['GET'])
+def fx_exchanges():
+    return requests.get('https://finnhub.io/api/v1/forex/exchange?token=' + api_key).content
+
+# @retrieve_bp.route('/fx_by_exchange', methods=['GET'])
+# def list_fx():

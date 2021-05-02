0349a68676e98d2599ad998dfe81e8b812cbc68d
Jonathan Choi <jisung594@gmail.com>
2021-05-02 11:02:46 -0500

    test

--- tracer_be/__init__.py
+++ tracer_be/__init__.py
@@ -49,7 +49,7 @@ def create_app():
     @app.after_request
     def after_request(response):
       response.headers.add('Access-Control-Allow-Origin', '*')
-      response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+      response.headers.add('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
       response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
       return response
 

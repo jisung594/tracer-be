fea25a71ced839fb9570d8aba66df1f365d0a628
Jonathan Choi <jisung594@gmail.com>
2021-05-02 11:57:34 -0500

    test

--- tracer_be/retrieve/retrieve.py
+++ tracer_be/retrieve/retrieve.py
@@ -21,11 +21,11 @@ CORS(retrieve_bp) #------------------------
 def list_stocks():
     # return requests.get('https://finnhub.io/api/v1/stock/symbol?exchange=US&token=' + api_key).content
     res = requests.get('https://finnhub.io/api/v1/stock/symbol?exchange=US&token=' + api_key).content
-    # res.headers['Content-Type'] = 'application/json'
-    # h = res.headers
-    # h['Access-Control-Allow-Origin'] = flask.request.environ['HTTP_ORIGIN']
-    # h['Access-Control-Allow-Methods'] = 'GET'
-    # h['Access-Control-Allow-Headers'] = 'X-Requested-With'
+    res.headers['Content-Type'] = 'application/json'
+    h = res.headers
+    h['Access-Control-Allow-Origin'] = flask.request.environ['HTTP_ORIGIN']
+    h['Access-Control-Allow-Methods'] = 'GET'
+    h['Access-Control-Allow-Headers'] = 'X-Requested-With'
     #
     # res.headers = h
     return res
@@ -37,7 +37,7 @@ def after_request(response):
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   return response
 
-  
+
 
 
 @retrieve_bp.route('/current_price', methods=['GET','POST'])

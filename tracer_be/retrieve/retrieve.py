c5c00b25ab6eafc1af1665e74739a4840893ada0
Jonathan Choi <jisung594@gmail.com>
2021-05-02 10:06:59 -0500

    test

--- tracer_be/retrieve/retrieve.py
+++ tracer_be/retrieve/retrieve.py
@@ -17,9 +17,11 @@ CORS(retrieve_bp, resources={r"/*": {"origins": "*"}},  supports_credentials=Tru
 
 # STOCKS
 @retrieve_bp.route('/stocks_us', methods=['GET'])
-@cross_origin(origin='http://127.0.0.1:3000',headers=['Content-Type','Authorization'])
+@cross_origin(origin='*',headers=['Content-Type','Authorization'])
 def list_stocks():
-    return requests.get('https://finnhub.io/api/v1/stock/symbol?exchange=US&token=' + api_key).content
+    # return requests.get('https://finnhub.io/api/v1/stock/symbol?exchange=US&token=' + api_key).content
+    res = requests.get('https://finnhub.io/api/v1/stock/symbol?exchange=US&token=' + api_key).content
+    return res.headers.add('Access-Control-Allow-Origin', '*')
 
 
 @retrieve_bp.route('/current_price', methods=['GET','POST'])
